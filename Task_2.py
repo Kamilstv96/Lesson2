02c704ae5ec8d7311afe09052ab74e7c3741b89d
Kamilstv96 <kamilstv96@mail.ru>
2022-05-15 22:17:14 +0300

    initial

---
+++ Task_2.py
@@ -0,0 +1,6 @@
+k = 0
+for i in range(10):
+    num = int(input())
+    if num == 5:
+        k += 1
+print(k)
---
+++ Task_3.py
@@ -0,0 +1,7 @@
+summ = 0
+
+for i in range(0, 100 + 1):
+
+   summ += i
+
+print(summ)
